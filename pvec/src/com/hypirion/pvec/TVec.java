74abacdf438bb27dc57a0a72cb07acedae13c81e
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-10-26 14:34:34 +0100

    Implement TVec.

--- pvec/src/com/hypirion/pvec/TVec.java
+++ pvec/src/com/hypirion/pvec/TVec.java
@@ -13,47 +13,64 @@
 
 package com.hypirion.pvec;
 
-public final class PVec {
-    private final int size;
-    private final int shift;
-    private final Object[] tail;
-    private final Object[] root;
+/* This implementation is essentially copypaste from PVec, but there are a
+   couple of "manual" changes which is done:
 
-    private static final Object[] EMPTY_TAIL = new Object[0];
+   - Cloning and node creating is replaced with ensureEditable() and newNode().
+   - Whenever a new TVec from a persistent vector, the tail is expanded.
+   - Whenever a TVec is converted back to a persistent vector, its tail is
+     compressed and the TVec cannot be used anymore.
+   - Instead of returning a new vector like PVec, it always returns itself.
+*/
 
-    public PVec() {
+public final class TVec {
+    private int size;
+    private int shift;
+    private Object[] tail;
+    private Object[] root;
+    private Object id;
+
+    public TVec() {
+        id = new Object();
         size = 0;
         shift = 0;
-        tail = EMPTY_TAIL;
+        tail = newNode(id);
         root = null;
     }
 
-    PVec(int size, int shift, Object[] root, Object[] tail) {
+    TVec(int size, int shift, Object[] root, Object[] tail) {
+        id = new Object();
         this.size = size;
         this.shift = shift;
-        this.tail = tail;
+        this.tail = expandNode(tail, id);
         this.root = root;
     }
 
-    public PVec set(int i, Object val) {
+    private static Object[] expandNode(Object[] node, Object id) {
+        Object[] expanded = new Object[33];
+        System.arraycopy(node, 0, expanded, 0, node.length);
+        expanded[32] = id;
+        return expanded;
+    }
+
+    public TVec set(int i, Object val) {
         rangeCheck(i);
         if (i >= tailOffset()) {
-            Object[] newTail = tail.clone();
-            newTail[i & 31] = val;
-            return new PVec(size, shift, root, newTail);
+            tail[i & 31] = val;
+            return this;
         }
         else {
-            Object[] newRoot = root.clone();
-            Object[] node = newRoot;
+            root = ensureEditable(root, id);
+            Object[] node = root;
             for (int level = shift; level > 0; level -= 5) {
                 int subidx = (i >>> level) & 31;
                 Object[] child = (Object[]) node[subidx];
-                child = child.clone();
+                child = ensureEditable(child, id);
                 node[subidx] = child;
                 node = child;
             }
             node[i & 31] = val;
-            return new PVec(size, shift, newRoot, tail);
+            return this;
         }
     }
 
@@ -71,40 +88,47 @@ public final class PVec {
         }
     }
 
-    public PVec push(Object val) {
+    public TVec push(Object val) {
         int ts = tailSize();
         if (ts != 32) {
-            Object[] newTail = new Object[ts+1];
-            System.arraycopy(tail, 0, newTail, 0, ts);
-            newTail[ts] = val;
-            return new PVec(size+1, shift, root, newTail);
+            tail[ts] = val;
+            size++;
+            return this;
         }
         else { // have to insert tail into root.
-            Object[] newTail = new Object[]{val};
+            Object[] newTail = newNode(id);
+            newTail[0] = val;
             // Special case: If old size == 32, then tail is new root
             if (size == 32) {
-                return new PVec(size+1, 0, tail, newTail);
+                size++;
+                root = tail;
+                tail = newTail;
+                return this;
             }
             // check if the root is completely filled. Must also increment
             // shift if that's the case.
-            Object[] newRoot;
-            int newShift = shift;
             if ((size >>> 5) > (1 << shift)) {
-                newShift += 5;
-                newRoot = new Object[32];
+                Object[] newRoot = newNode(id);
                 newRoot[0] = root;
-                newRoot[1] = newPath(shift, tail);
-                return new PVec(size+1, newShift, newRoot, newTail);
+                newRoot[1] = newPath(shift, tail, id);
+                shift += 5;
+                size++;
+                root = newRoot;
+                tail = newTail;
+                return this;
             }
             else { // still space in root
-                newRoot = pushLeaf(shift, size-1, root, tail);
-                return new PVec(size+1, shift, newRoot, newTail);
+                root = pushLeaf(shift, size-1, root, tail, id);
+                tail = newTail;
+                size++;
+                return this;
             }
         }
     }
 
-    private static Object[] pushLeaf(int shift, int i, Object[] root, Object[] tail) {
-        Object[] newRoot = root.clone();
+    private static Object[] pushLeaf(int shift, int i, Object[] root,
+                                     Object[] tail, Object id) {
+        Object[] newRoot = ensureEditable(root, id);
         Object[] node = newRoot;
         for (int level = shift; level > 5; level -= 5) {
             int subidx = (i >>> level) & 31;
@@ -114,10 +138,10 @@ public final class PVec {
             // but we'll still have to assign node[subidx].
             // The null check should therefore be a bit faster.
             if (child == null) {
-                node[subidx] = newPath(level - 5, tail);
+                node[subidx] = newPath(level - 5, tail, id);
                 return newRoot;
             }
-            child = child.clone();
+            child = ensureEditable(child, id);
             node[subidx] = child;
             node = child;
         }
@@ -125,50 +149,59 @@ public final class PVec {
         return newRoot;
     }
 
-    private static Object[] newPath(int levels, Object[] tail) {
+    private static Object[] newPath(int levels, Object[] tail, Object id) {
         Object[] topNode = tail;
         for (int level = levels; level > 0; level -= 5) {
-            Object[] newTop = new Object[32];
+            Object[] newTop = newNode(id);
             newTop[0] = topNode;
             topNode = newTop;
         }
         return topNode;
     }
 
-    public PVec pop() {
+    public TVec pop() {
         rangeCheck(0);
         if (size == 1) {
-            return new PVec();
+            size = 0;
+            tail[0] = null;
+            return this;
         }
         int ts = tailSize();
         if (ts > 1) {
-            Object[] newTail = new Object[ts-1];
-            System.arraycopy(tail, 0, newTail, 0, ts-1);
-            return new PVec(size-1, shift, root, newTail);
+            tail[ts] = null;
+            size--;
+            return this;
         }
         else { // has to find new tail
             int newTrieSize = size - 33;
             // special case: if new size is 32, then new root turns is null, old
             // root the tail
             if (newTrieSize == 0) {
-                return new PVec(32, 0, null, root);
+                shift = 0;
+                size = 32;
+                tail = root;
+                root = null;
+                return this;
             }
             // check if we can reduce the trie's height
             if (newTrieSize == 1 << shift) { // can lower the height
-                int lowerShift = shift - 5;
+                shift -= 5;
                 Object[] newRoot = (Object[]) root[0];
 
                 // find new tail
                 Object[] node = (Object[]) root[1];
-                for (int level = lowerShift; level > 0; level -= 5) {
+                for (int level = shift; level > 0; level -= 5) {
                     node = (Object[]) node[0];
                 }
-                return new PVec(size-1, lowerShift, newRoot, node);
+                size--;
+                root = newRoot;
+                tail = node;
+                return this;
             } else { // height is same
                 // diverges contain information on when the path diverges.
                 int diverges = newTrieSize ^ (newTrieSize - 1);
                 boolean hasDiverged = false;
-                Object[] newRoot = root.clone();
+                Object[] newRoot = ensureEditable(root, id);
                 Object[] node = newRoot;
                 for (int level = shift; level > 0; level -= 5) {
                     int subidx = (newTrieSize >>> level) & 31;
@@ -180,12 +213,15 @@ public final class PVec {
                         node[subidx] = null;
                         node = child;
                     } else {
-                        child = child.clone();
+                        child = ensureEditable(child, id);
                         node[subidx] = child;
                         node = child;
                     }
                 }
-                return new PVec(size-1, shift, newRoot, node);
+                root = newRoot;
+                tail = node;
+                size--;
+                return this;
             }
         }
     }
@@ -194,13 +230,18 @@ public final class PVec {
         return size;
     }
 
-    public PVec map(Fun f) {
+    public TVec map(Fun f) {
         return null;
     }
 
     private void rangeCheck(int index) {
         if (index >= size)
             throw new IndexOutOfBoundsException("Index:"+index+", Size:"+size);
+    }
+
+    private void editCheck() {
+        if (id == null)
+            throw new IllegalStateException("Transient has been converted to persistent.");
     }
 
     private int tailOffset() {
@@ -214,7 +255,34 @@ public final class PVec {
             return ((size-1) & 31)+1;
     }
 
-    public TVec asTransient() {
-        return new TVec(size, shift, root, tail);
+    public PVec asPersistent() {
+        id = null;
+        return new PVec(size, shift, root, compressedTail());
+    }
+
+    private Object[] compressedTail() {
+        int ts = tailSize();
+        Object[] compressed = new Object[ts];
+        System.arraycopy(tail, 0, compressed, 0, ts);
+        return compressed;
+    }
+
+    private static Object[] newNode(Object id) {
+        Object[] node = new Object[33];
+        node[32] = id;
+        return node;
+    }
+
+    private static Object[] ensureEditable(Object[] node, Object id) {
+        if (node.length == 33 && node[32] == id) {
+            return node;
+        }
+        else {
+            Object[] editable = new Object[33];
+            // this arraycopy assumes nodes cannot be more than 33 elts long
+            System.arraycopy(node, 0, editable, 0, node.length);
+            editable[32] = id;
+            return editable;
+        }
     }
 }
