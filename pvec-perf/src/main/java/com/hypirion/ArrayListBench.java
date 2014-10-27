7dd076c6aef8fdb6d0b4915736dc122d374bee76
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-10-27 23:09:24 +0100

    Initial Maven project for JMH benchmarks.

---
+++ pvec-perf/src/main/java/com/hypirion/ArrayListBench.java
@@ -0,0 +1,12 @@
+package com.hypirion;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class ArrayListBench {
+
+    @Benchmark
+    public void benchUpdate() {
+        // yes yes, I'll get to it.
+    }
+
+}
