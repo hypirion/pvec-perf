ed6c76f190e3adb4df5ff9aea3b2c40dff4c5b4e
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-10-28 23:34:18 +0100

    ArrayList update bench.

---
+++ pvec-perf/src/main/java/com/hypirion/bench/arraylist/Update.java
@@ -0,0 +1,37 @@
+package com.hypirion.bench.arraylist;
+
+import java.util.ArrayList;
+import java.util.Random;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Scope;
+
+@State(Scope.Benchmark)
+public class Update {
+
+    @Param({"0", "1", "2", "3", "4", "5"})
+    public int bits;
+    int size;
+
+    Random r;
+
+    ArrayList al;
+
+    @Setup
+    public void setup() {
+        size = 1 << (5*bits);
+        r = new Random(1);
+        al = new ArrayList();
+        for (int i = 0; i < size; i++) {
+            al.add(new Object());
+        }
+    }
+
+    @Benchmark
+    public void benchUpdate() {
+        al.set(r.nextInt(size), new Object());
+    }
+
+}
